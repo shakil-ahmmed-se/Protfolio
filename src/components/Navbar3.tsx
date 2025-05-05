f1621755f01067f0c6fcd84d96dc291b28eb2585
shakil-ahmmede-se <mdshakilahmmed8906@gmail.com>
2025-05-05 12:39:43 +0600

    update navbar

--- src/components/Navbar3.tsx
+++ src/components/Navbar3.tsx
@@ -79,7 +79,7 @@ const Navbar3 = () => {
           : "bg-transparent"
       }`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 md:h-20">
           {/* Logo */}
           <motion.div
